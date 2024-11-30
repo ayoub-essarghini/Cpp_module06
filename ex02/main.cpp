2f550a3b7edf41940c6854c8704b7f0ea4fb5ace
roboto <ayoubmajd311@gmail.com>
2024-11-30 19:03:35 +0100

    enhance tests in main

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,34 +1,23 @@
 #include "includes/Identify.hpp"
 
-int main() {
+int main()
+{
 
+ 
 
-
-Base* base = new A();
-
-Base& ref = *base;
-
-    try{
-        dynamic_cast<B&>(ref);
-        std::cout << "done" << std::endl;
-    }
-    catch(std::exception& e)
     {
-        std::cout << e.what() << std::endl;
+        Base *random = generate();
+        std::cout << "Pointer test: ";
+        identify(random);
+        delete random;
     }
-    // {
-    //     Base* random = generate();
-    //     std::cout << "Pointer test: ";
-    //     identify(random); 
-    //     delete random;
-    // }
 
-    // {
-    //     Base* random = generate();
-    //     Base& base = *random;
-    //     std::cout << "Reference test: ";
-    //     identify(base);
-    //     delete random;
-    // }
+    {
+        Base *random = generate();
+        Base &base = *random;
+        std::cout << "Reference test: ";
+        identify(base);
+        delete random;
+    }
     return 0;
 }
